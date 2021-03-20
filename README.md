3b013536ec27300dfede63276e333f36d51c15d6
katai5plate <katai5plate.sub@gmail.com>
2021-03-21 04:03:11 +0900

    add user-mode

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ RPG2000 / steam2003 の戦闘アニメ JSON データ
 
 ## どうやって取得したか
 
-1. [戦闘アニメ・エクセル相互変換ツール](https://fermiumbay13.hatenablog.com/entry/2017/12/04/221051) を落とす
-2. 手順通りにエクセルのクリップボードを作成し、テキストとして`./{2000,2003s}/*.2kxanm`に保存 (SJIS)
-3. `npm run convert` を実行
+1. [戦闘アニメ・エクセル相互変換ツール](https://fermiumbay13.hatenablog.com/entry/2017/12/04/221051) を使う
+2. 手順通りにエクセルのクリップボードを作成し、テキストとして `./{2000,2003s}/*.2kxanm` に保存
+3. `npm run convert-sjis` または `npm run convert-utf8` を実行
+
+## 自分で任意の戦闘アニメ JSON を作成する手順
+
+1. このリポジトリを clone してきて、`npm i` する
+2. [戦闘アニメ・エクセル相互変換ツール](https://fermiumbay13.hatenablog.com/entry/2017/12/04/221051) を使う
+3. 手順通りにエクセルのクリップボードを作成
+4. 内容をメモ帳などで `./user` に `{名前}.2kxanm` として保存
+5. 文字コードに応じて、`npm run convert-sjis-user` または `npm run convert-utf8-user` を実行
--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const fs = require("fs");
 const { parse, dirname } = require("path");
 const jsonGenerator = require("./jsonGenerator");
-const files = [
-  ...fs
-    .readdirSync("./2000")
-    .filter((x) => /2kxanm$/.test(x))
-    .map((x) => "./2000/" + x),
-  ...fs
-    .readdirSync("./2003s")
-    .filter((x) => /2kxanm$/.test(x))
-    .map((x) => "./2003s/" + x),
-];
 
-files.forEach((file, i, { length }) => {
-  fs.writeFileSync(
-    dirname(file) + "/" + parse(file).name + ".json",
-    JSON.stringify(jsonGenerator(file), null, 2)
-  );
-  console.log(i, "/", length - 1, file);
-});
+module.exports = (encode, userOnly = false) => {
+  const files = [
+    ...(userOnly
+      ? []
+      : [
+          ...fs
+            .readdirSync("./2000")
+            .filter((x) => /2kxanm$/.test(x))
+            .map((x) => "./2000/" + x),
+          ...fs
+            .readdirSync("./2003s")
+            .filter((x) => /2kxanm$/.test(x))
+            .map((x) => "./2003s/" + x),
+        ]),
+    ...fs
+      .readdirSync("./user")
+      .filter((x) => /2kxanm$/.test(x))
+      .map((x) => "./user/" + x),
+  ];
+
+  files.forEach((file, i, { length }) => {
+    fs.writeFileSync(
+      dirname(file) + "/" + parse(file).name + ".json",
+      JSON.stringify(jsonGenerator(file, encode), null, 2)
+    );
+    console.log(i, "/", length - 1, file);
+  });
+};
